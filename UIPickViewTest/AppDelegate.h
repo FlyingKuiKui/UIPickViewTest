e4c9fb41ebe8c5deb1a4abb62000fee584cb0421
王盛魁 <wangshengkui@wangshengkuidemacbook-air.local>
2016-02-23 15:12:51 +0800

    Initial Commit

---
+++ UIPickViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIPickViewTest
+//
+//  Created by 王盛魁 on 16/2/23.
+//  Copyright © 2016年 wangsk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
